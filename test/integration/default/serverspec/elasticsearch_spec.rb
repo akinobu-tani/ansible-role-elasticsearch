4791236901a769616255f53086b757cd0c6fae18
akinobu-tani <akinobu.x.tani@gmail.com>
2017-06-25 02:55:57 +0900

    クラスタ名を設定可能にする

--- test/integration/default/serverspec/elasticsearch_spec.rb
+++ test/integration/default/serverspec/elasticsearch_spec.rb
@@ -4,3 +4,7 @@ describe service('elasticsearch') do
   it { should be_running }
   it { should be_enabled }
 end
+
+describe command('curl http://localhost:9200') do
+  its(:stdout) { should match /"cluster_name" : "elasticsearch"/ }
+end
